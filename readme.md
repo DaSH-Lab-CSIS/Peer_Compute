e53d44b873f05a2756a3738cd174434c339cc30a
aalhadsawane <aalhadsawane@gmail.com>
2024-09-19 04:06:30 +0530

    recommit

---
+++ readme.md
@@ -0,0 +1,207 @@
+example curl
+```
+
+  curl -X POST "http://localhost:8000/developers/run_service/5" \
+  -H "Accept: */*" \
+  -H "User-Agent: Thunder Client (https://www.thunderclient.com)" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "numberOfInvocations": 1,
+	"chained": false,
+	"input": "None",
+	"runMultipleInvocations": false
+  }'
+```
+```
+  curl -X GET "http://localhost:8000/providers/calculate_efficiency/34933555-5cca-41fb-aded-4ab7900c48d5" 
+```
+```
+curl -X POST "http://localhost:8000/providers/set_reference_stats_for_service/" -H "Content-Type: application/json" -d '{"service_id":"satyam098/testimage_largeruntime"}'
+```
+```
+  curl -X GET "http://localhost:8000/providers/calculate_efficiency/34933555-5cca-41fb-aded-4ab7900c48d5" 
+```
+```
+curl -X POST "http://localhost:8000/providers/set_reference_stats_for_service/" -H "Content-Type: application/json" -d '{"service_id":"satyam098/testimage_largeruntime"}'
+```
+services:
+run_service/3 - (hello world),  
+run_service/5 - (largeruntime),
+run_service/6 - video_encoding benchmark from vswarmease.
+reference provider: ```34933555-5cca-41fb-aded-4ab7900c48d5```
+
+## TODO
+Set_reference_stats_for_service/<str: service_id> Here service_id is not the service_id but instead the task link. Get the task link from this service id in views.py/provider itself, use that instead and continue remaining without any changes.   
+Make a copy of provider1.py without set_reference_stats and other exclusive reference provider methods. This script will be for all other nodes.  
+Remove try and except instead give if statements and startswith(dockerrun) etc
+
+## Common Issues:
+
+NOTE: Rerun the provider script after running the benchmark. The efficiency scores are not fetched in the global vars in provider1.py after calling benchmark function, but rather only at the start of provider1.py script. So since before running benchmarks global vars were nothing and efficiency score get request is done after running benchmarks eff scores are not in global vars for this first benchmark run. Fix this.
+
+if this provider does not write "Connected successfully", contact me (Aalhad). 
+paste the IP I give into the global variable named "BROKER_ID" in provider1.py and providers/views.py
+If hivemq not connecting check internet with curl parrot.live
+
+If Django server Port already in use then close then use a different port, with
+```python scheduler/manage.py runserver 0.0.0.0:5000```
+and also change the url in curl requests.
+
+For mac, brew info mosquitto to locate conf file.
+
+If Django server not working saying is port 5432 accepting tcp ip connections and postgres not opening chainfaas then just restart postgres with ```sudo systemctl start postgresql``` followed by ```sudo systemctl enable postgresql```
+
+If test\n container already in use error, set the global var "container_name" to something different than what it is rn ex "test20" -> "test21"
+
+If Django server is on a infinite loop with finding ready providers. One of th providers might not be ready make it ready by:
+SQL command for making provider ready (if id 14 does not have t and t in the table)
+```
+UPDATE profiles_user
+SET memory_efficiency_score = 1
+WHERE id = '14';
+```
+
+If docker container is not being loaded from the registry or some certificate issue u are running into, you are not connected to the wifi.
+There is a python login script in the Documents folder name loginscript.py or login.py run that with ```python login.py```
+
+If trainAndPredict fails, check the data in TrainingData, none of the eff score should be "could not load" all should be floats.
+
+## Automate the startup terminals
+Install the extension Terminals Manager by Fabio Spampinato
+type in "terminals edit configuration" in command pallete (cmd+shift+P)
+and replace it with the json in the end of this readme. 
+
+Now type in "terminals run" in command pallete to run the startup terminals
+
+In the postgres terminal type in password and enter the following:
+```
+psql chainfaas
+select * from profiles_user;
+
+```
+Press q to exit table.
+
+The JSON:
+```
+{
+  "autorun": false,
+  "terminals": [
+    {
+      "name": "Postgres",
+      "description": "This is a description",
+      "commands": ["cd ~/Documents/Serverless_Scheduler", "deactivate", "source .venv/bin/activate", "sudo -i -u postgres"]
+    },
+    {
+      "name": "Django Server",
+      "commands": [
+        "cd ~/Documents/Serverless_Scheduler", "deactivate", "source .venv/bin/activate", "python scheduler/manage.py runserver 0.0.0.0:8000"
+      ]
+    },
+    {
+      "name": "Python Provider script",
+      "focus": true,
+      "execute": false,
+      "commands": [
+        "cd ~/Documents/Serverless_Scheduler", "deactivate", "source .venv/bin/activate","python provider/provider1.py 34933555-5cca-41fb-aded-4ab7900c48d5"
+      ]
+    },
+    {
+      "name": "Curl Requests",
+      "command": "# Enter curl requests here. There's an example in startup.md"
+    }
+  ]
+}
+```
+
+# Local Installation
+
+## Setting virutal environments.
+
+Make a virtual environment named ".venv" and make one named "chainenv", both in the base folder (Serverless_Scheduler)
+```
+pip install virtualenv
+python3 -m venv .venv
+```
+
+now activate .venv and install requirements.
+```
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+
+after installation is done deactivate this virtual env by typing `deactivate` , and use `deactivate` everytime u switch virtual env.
+```
+deactivate
+```
+now activate chainenv and install requirements
+```
+source chainenv/bin/activate
+pip install -r requirements_chain.txt
+```
+after installation `deactivate`.
+
+## Changing IPs of  providers.
+
+use 
+```
+ipconfig getifaddr en0
+```
+to get the ip of your machine.
+Put this in the global var `controller_ip` of provider1.py
+
+NOTE: Do not worry about broker part it will run even if u run this locally.
+More info: right now the mqtt broker is hosted on a cloud based public node with IP: "broker.hivemq.com".
+this is a free broker host which allows everyone. otherwise the broker host would be one of the lab machines with a custom config. (allow_anonymous true \n listener 1883)
+
+
+## Things to fix at some point.
+
+These work but will be inefficient at scale.  
+Everytime startup request is sent, it creates a new client and a new mqtt daemon subbed to "EVERYONE".
+Instead a new client should be made and subbed only at django server startup (lookup ready method in apps.py)
+this client should then be used in all other methods. So make a method mclient = get_mclient() which has a global var.
+
+To change the reference provider, change value in benchmark_results.txt and the global var in views.py/provider.
+
+
+## Room for improvement
+
+### 1. Runtime prediction model not specific to service
+
+Runtime of some service on p1 is predicted by a linear function f:
+
+$predicted\_runtime = f(cpu\_usage\_on\_reference * cpu\_efficiency\_of\_p1,\text{ } memory\_usage\_on\_reference * memory\_efficiency\_of\_p1)$
+
+where cpu_usage_on_reference and memory_usage_of_reference are usages of that service on reference providers.
+We know that this linear function, f , should ideally be different for each service. 
+perhaps some services give runtime more on the basis of memory_stats than other who have heavier weightage for cpu_stats.
+We could train a linear regression model for each service and get f specific to the service.
+But at scale if PeerCompute has thousands of services in the registry and thousands of providers. For the training of these models,
+each function would have to be run on a lot of providers.   
+Solution: Take a set of providers which represent diverse fast and slow providers. Run all services on them and based on that make a model for each service.  
+Issue: This goes against the concept of decentralisation, like who gets to be in this set of model making providers.  
+Better Solution: Divide all services into categories like compute-heavy, memory-heavy, both-heavy, io-heavy etc and make a prediction model for each category
+
+##### DB on .48
+Django settings:
+```
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'chainfaas',
+        'USER':'chainfaas_dba',
+        'PASSWORD':'password',
+        'HOST':'localhost',
+        'PORT':'5432',
+    }
+}
+```
+
+To clear the diskCache use
+```
+from diskcache import Cache
+
+cache = Cache('cache_dir')  # Initialize your cache
+cache.clear()  # Clear the entire cache
+print("Disk cache cleared.")
+```
--- scheduler/providers/views.py
+++ scheduler/providers/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404, redirect
 import pika 
 import json
+import socket
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from providers.forms import ProviderForm
@@ -12,13 +13,152 @@ from django.http import JsonResponse
 from scheduler.settings import TIME_ZONE
 from pytz import timezone
 from django.contrib import messages
-import zmq
+import paho.mqtt.client as mqtt
+import queue
+from profiles.models import User
+from providers.models import Job
+from random import randint 
+from scheduler.settings import USE_FABRIC
+import fabric.views as fabric
+import csv
+import random
+from providers.mincost import minimize_total_cost
 # Create your views here.
-zmq_context = zmq.Context()
+data_dict = None
+BROKER_ID = "broker.hivemq.com"
+reference_provider_id = '34933555-5cca-41fb-aded-4ab7900c48d5'
+file_path = "/home/user/Documents/Serverless_Scheduler/SchedInfo.csv"
 
-def publish_to_topic(runMultipleInvocations, numberOfInvocations, isChained, inputData, provider , task_link , task_developer, job_id):
+mclient = None
+global service_id_array 
+global service_queue
+service_id_array = {}
+service_queue = queue.Queue()
+global requested_services
+
+# Helpers
+def load_data_as_dict(file_path):
+    all_data = {}
+    with open(file_path, 'r') as file:
+        for line in file:
+            data_dict = json.loads(line.strip())
+            all_data.update(data_dict)
+    return all_data
+
+def update_efficiency_score_in_models(user_id, provider_cpu_usage, provider_memory_usage, reference_cpu_usage, reference_memory_usage):
+    print("updating models... with r_cpu= ", reference_cpu_usage, " r_mem= ", reference_memory_usage, " p_cpu= ", provider_cpu_usage, " p_mem= ", provider_memory_usage)
+    # uncomment after adding column to database tables
+    provider= User.objects.get(user_id=user_id)
+    provider.cpu_efficiency_score = provider_cpu_usage/reference_cpu_usage
+    provider.memory_efficiency_score = provider_memory_usage/reference_memory_usage
+    provider.save()
+
+def get_benchmarks_for(user_id, benchmark):
+    benchmarks = load_data_as_dict("benchmark_results.txt")
+    reference_stats=benchmarks[benchmarks['Reference']]
+    reference_cpu_usage= reference_stats['cpu_usage']
+    reference_memory_usage= reference_stats['memory_usage']
+    provider_cpu_usage = benchmark[user_id]['cpu_usage']
+    provider_memory_usage = benchmark[user_id]['memory_usage']
+    print(reference_stats)
+    update_efficiency_score_in_models(user_id,provider_cpu_usage, provider_memory_usage, reference_cpu_usage, reference_memory_usage)
+
+# mqtt client callbacks:
+def on_connect(mqtt_client, userdata, flags, rc, callback_api_version):
+    mqtt_client.subscribe(topic="EVERYONE")
+    print("Connected from views.py/providers")
+
+def on_message(mqtt_client, userdata, msg):
+    print('from views.py/providers ')
+    print(f'Received message on topic: {msg.topic} with payload: {msg.payload}')
+    try:
+        data = json.loads(msg.payload.decode("utf-8"))
+        if(data['stage'] == 'dockernotrun'): print("pulled but docker not run")
+        if(data['stage'] == 'dockerrun'):
+            print(f"data[stage]==dockerrun works")
+            print(data)
+            finish_job(data)
+            # mqtt_client.loop_stop()
+            # mqtt_client.disconnect()
+            # receive_job_data(data_dic t)
+    except:
+        print(msg.topic,msg.payload.decode("utf-8"))
+        if(msg.payload.decode("utf-8").startswith("Benchmark:")):
+            # this is in topic user_id not EVERYONE
+            print("In except, will print benchmark...")
+            benchmark = json.loads(msg.payload.decode("utf-8")[10:])
+            user_id = list(benchmark.keys())[0]
+            get_benchmarks_for(user_id=user_id, benchmark=benchmark) #this will also update models.
+        elif(msg.payload.decode("utf-8").startswith("Stats for Reference Provider: ")):
+            print("Stats added to TrainingData/Reference_Provider_Data.txt")
+        if(msg.topic=="EVERYONE"):
+            if(msg.payload.decode("utf-8").startswith("start_connect")):
+                print("connecting to ", msg.payload.decode("utf-8")[13:])
+                mqtt_client.subscribe(topic=msg.payload.decode("utf-8")[13:])
+            if(msg.payload.decode("utf-8").startswith("get_efficiency_score")):
+                
+                user_id=msg.payload.decode("utf-8")[20:]
+                provider = User.objects.get(user_id=user_id)
+                scoreset = {'cpu':float(provider.cpu_efficiency_score), 'memory':float(provider.memory_efficiency_score)}
+                mqtt_client.publish(topic=user_id, payload="EfficiencyScoreSet:"+json.dumps(scoreset),qos=2)
+            
+
+def on_subscribe(mqtt_client, userdata, mid, qos, properties=None):
+    print("on_subscribe userdata is "+ str(mqtt_client))
+
+def get_mclient():
+    global mclient
+    if(mclient == None):
+        mclient = mqtt.Client(callback_api_version= mqtt.CallbackAPIVersion.VERSION2)
+        mclient.on_connect = on_connect
+        mclient.on_message = on_message
+        mclient.on_subscribe= on_subscribe
+        mclient.connect(host=BROKER_ID,port=1883)
+        mclient.loop_start()
+    return mclient
+
+# mqtt global communications, all providers are subbed to this topic and the schedule is too
+# TODO This stuff is not called by any url pattern.
+
+############################################################################################
+
+# def publish_to_topic(runMultipleInvocations, numberOfInvocations, isChained, inputData, provider , task_link , task_developer, job_id):
+#     router_name = str(provider.user_id)
+#     print("publish_to_topic used NOT MQTT")
+#     zmq_data = {
+#         'provider_id' : router_name,
+#         'task_link' : task_link,
+#         'task_developer' : str(task_developer.user_id),
+#         'job_id' : job_id,
+#         'numberOfInvocations': numberOfInvocations,
+#         'isChained': isChained,
+#         'inputData': inputData,
+#         'runMultipleInvocations': runMultipleInvocations
+#     }
+#     zmq_socket = zmq_context.socket(zmq.DEALER)
+#     dealer_id = b"dealer1"
+#     zmq_socket.setsockopt(zmq.IDENTITY, dealer_id)
+#     zmq_socket.bind("tcp://*:5555")
+#     # print("Sending zmq data.")
+#     zmq_socket.send_multipart([router_name.encode("utf-8"), json.dumps(zmq_data).encode("utf-8")])
+#     # print("Waiting for zmq response.")
+#     response = zmq_socket.recv()
+#     # print("Received response from zmq: ", response)
+#     zmq_socket.close()
+#     return response
+
+
+def findfreq_service(service):
+    while(service_queue.qsize()>=30):
+        service_id_array[service_queue.get()] -=1
+    service_queue.put(service.id)
+    service_id_array[service.id] = service_id_array[service.id]+1
+    return
+
+# pub to topic mqtt actually just forwards it to provider1.py where it adds pull times and stuff and then it publishes.
+def publish_to_topic_mqtt(runMultipleInvocations, numberOfInvocations, isChained, inputData, provider , task_link , task_developer, job_id):
     router_name = str(provider.user_id)
-    zmq_data = {
+    userdata = {
         'provider_id' : router_name,
         'task_link' : task_link,
         'task_developer' : str(task_developer.user_id),
@@ -26,16 +166,21 @@ def publish_to_topic(runMultipleInvocations, numberOfInvocations, isChained, inp
         'numberOfInvocations': numberOfInvocations,
         'isChained': isChained,
         'inputData': inputData,
-        'runMultipleInvocations': runMultipleInvocations
-    }
-    zmq_socket = zmq_context.socket(zmq.DEALER)
-    dealer_id = b"dealer1"
-    zmq_socket.setsockopt(zmq.IDENTITY, dealer_id)
-    zmq_socket.bind("tcp://*:5555")
-    zmq_socket.send_multipart([router_name.encode("utf-8"), json.dumps(zmq_data).encode("utf-8")])
-    response = zmq_socket.recv()
-    zmq_socket.close()
-    return response
+        'runMultipleInvocations': runMultipleInvocations,
+        'stage': "dockernotrun"
+    }  
+    #makes a new client everytime it pubtotopic is called.
+    mclient = get_mclient()
+    mclient.subscribe(topic=router_name)
+    mclient.publish(topic=router_name, payload=json.dumps(userdata).encode("utf-8"), qos=2)
+    print("in pub to topic mqtt")
+    # mclient.loop_forever() #get rid of this
+    # dont return this return the data which is sent by on_message {data} which is stored in global var called data_dict
+    
+    # IMP # 
+    #return json.dumps(data_dict)
+    return
+
 
 
 # def make_rmq_user(user):
@@ -136,7 +281,37 @@ def job_ack(request, job_id):
         messages.error(request, "Wrong request method.")
     return JsonResponse({'message' : 'Job acknowledge time updated successfully.'})
 
+def calculate_efficiency(request, user_id):
+    #TODO
+    # send this to provider1.py requesting a docker container value, update it to the provider model.
+    client = get_mclient()
+    client.subscribe(topic=user_id)
+    client.publish(topic=user_id, payload="calculate_efficiency", qos=2)
+    print("in calculate_efficiency")
+    print("views.py/provider loop_forever exited")
+    return JsonResponse({'State':'Updated new efficiency scores in database'})
 
+def providerStartup(request, user_id):
+    print("Provider ", user_id, " started...")
+    client = get_mclient()
+    #subscribe to EVERYONE in on_connect
+    client.subscribe(topic=user_id)
+    return JsonResponse({'State':'scheduler connected to provider user_id'})
+
+@csrf_exempt
+def set_reference_stats(request):
+    # send msg to reference provider with service id. on_msg of provider will call a function to execute this service.
+    # It will also add cpu_usage and memory_usage to a txt file.
+    print("in set rstats (views/provider)")
+    client=get_mclient()
+    #subscribe to EVERYONE in on_connect
+    client.subscribe(topic=reference_provider_id)
+    service_id = json.loads(request.body.decode("utf-8"))['service_id']
+    print(type(service_id))
+    print(service_id)
+    # convert this service_id to task link before publishing. # Rn, we are actually directly passing in task link only.
+    client.publish(topic=reference_provider_id, payload="ref_run_service_id/"+service_id)
+    return JsonResponse({'State':'Running service on the reference provider, stats will be printed on django server and added to files also'})
 # class RpcClient(object):
 #     """
 #     This is the rabbitmq RpcClient class.
@@ -179,3 +354,165 @@ def job_ack(request, job_id):
 #         while self.response is None:
 #             self.connection.process_data_events()
 #         return self.response
+
+
+def request_handler(data,service,start_time,run_async = False):
+    print("In request handler.")
+    provider = None
+    while(provider == None):
+        provider = find_provider(service)
+    print(provider)
+    
+    job = Job.objects.create(provider = provider, start_time = start_time)
+    job.save()
+
+    if USE_FABRIC:
+        r = fabric.invoke_new_job(str(job.id), str(service.id), str(service.developer_id),
+                                        str(provider.id), provider_org="Org1")
+        if 'jwt expired' in r.text or 'jwt malformed' in r.text or 'User was not found' in r.text:
+            token = fabric.register_user()
+            r = fabric.invoke_new_job(str(job.id), str(service.id), str(service.developer_id),
+                                        str(provider.id), provider_org="Org1",token=token)
+
+    task_link = service.docker_container 
+    task_developer = service.developer
+    input_val = data['input']
+    response_decoded = None
+    # if(data['chained'] == True): 
+    #     for i in range(data['numberOfInvocations']):
+    #         response = publish_to_topic(data['runMultipleInvocations'], data['numberOfInvocations'], data['chained'], input_val, provider,task_link,task_developer, job.id)
+    #     # total_time = response['pull_time'] + response['run_time']
+    #         response_decoded = json.loads(response.decode("utf-8"))
+    #         input_val = int(response_decoded['Result'])
+    # for i in range(data['numberOfInvocations']):
+    #response = publish_to_topic(data['runMultipleInvocations'], data['numberOfInvocations'], data['chained'], input_val, provider,task_link,task_developer, job.id)
+    #print("abt to pub to mqtt")
+     # add the below code in on_message.
+    publish_to_topic_mqtt(data['runMultipleInvocations'], data['numberOfInvocations'], data['chained'], input_val, provider,task_link,task_developer, job.id)
+    # response_decoded = json.loads(response.decode("utf-8"))
+    # # response_decoded = json.loads(response)
+    # print("response from provider: ", response_decoded)
+    # job.refresh_from_db()
+    # job.pull_time = response_decoded['pull_time']
+    # job.run_time = response_decoded['run_time']
+    # job.total_time = response_decoded['total_time']
+    # job.cost = (response_decoded['total_time'])
+    # job.response = response_decoded['Result']
+    # job.finished = True
+    # job.save()
+    # providing_time = int(((job.ack_time - job.start_time)/timedelta(microseconds=1))/1000) # Providing time in milliseconds
+    # if USE_FABRIC:
+    #     r = fabric.invoke_received_result(str(job.id))
+    #     if 'jwt expired' in r.text or 'jwt malformed' in r.text or 'User was not found' in r.text:
+    #         token = fabric.register_user()
+    #         r = fabric.invoke_received_result(str(job.id), token=token)
+    # return response_decoded, provider.id, providing_time, str(job.id)
+
+
+def finish_job(data):
+    print("Inside finish job")
+    #here unpack the data load job from job id and update and save it.
+    id = data['job_id']
+    job = Job.objects.get(pk=id)
+    response_decoded = json.loads(data.decode("utf-8"))
+    # response_decoded = json.loads(response)
+    print("response from provider: ", response_decoded)
+    job.refresh_from_db()
+    job.pull_time = response_decoded['pull_time']
+    job.run_time = response_decoded['run_time']
+    job.total_time = response_decoded['total_time']
+    job.cost = (response_decoded['total_time'])
+    job.response = response_decoded['Result']
+    job.finished = True
+    job.save()
+    providing_time = int(((job.ack_time - job.start_time)/timedelta(microseconds=1))/1000) # Providing time in milliseconds
+    if USE_FABRIC:
+        r = fabric.invoke_received_result(str(job.id))
+        if 'jwt expired' in r.text or 'jwt malformed' in r.text or 'User was not found' in r.text:
+            token = fabric.register_user()
+            r = fabric.invoke_received_result(str(job.id), token=token)
+    return
+
+def queue_jobs(service):
+    requested_services.append(service)
+    
+
+def find_provider(service):
+
+    ready_providers = User.objects.filter(
+        active = True , ready = True , 
+        # last_ready_signal__gte = datetime.now(tz=timezone(TIME_ZONE)) - timedelta(minutes=10000)
+    )
+
+    print("Ready Providers: \n", ready_providers)
+    
+    if len(ready_providers) == 0: 
+        return
+
+    max_provider = None
+    max_invocations = -1
+    provider_choices= []
+
+    for provider_to_search in ready_providers:
+        with open(file_path, mode='r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                provider = row['Provider']
+                function = int(row['Function'])
+                invocations = int(row['Invocations'])
+
+                # Check if the row matches the criteria
+                if provider == str(provider_to_search.user_id) and function == (service.id - 7):
+                    provider_choices.append({'invocations': invocations, 'provider': provider_to_search.id})
+                    # max_provider = provider
+                    # max_invocations = invocations
+
+    # sort
+    if (len(provider_choices)< 1) :
+        max_provider = random.choice(ready_providers)
+
+    elif (len(provider_choices)==1):
+        max_provider = get_object_or_404(User, pk=provider_choices[0]['provider'])
+    else:
+        provider_choices.sort(key=lambda x: x['invocations'], reverse=True)
+        max_provider = get_object_or_404(User, pk = random.choice(provider_choices[0:2])['provider'])
+
+    print("Scheduler is chosing this provider -> ", max_provider)
+    updated_data = []
+    flag = False
+    if(max_provider != None):
+        # Read the CSV file and update the values
+        with open(file_path, mode='r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                provider = row['Provider']
+                function = int(row['Function'])
+                invocations = int(row['Invocations'])
+
+                # Check if the row matches the criteria for update
+                if provider == str(max_provider.user_id) and function == (service.id - 7):
+                    flag = True
+                    row['Invocations'] = str(int(invocations)+1)
+
+                updated_data.append(row)
+
+    if(flag == False):
+        updated_data.append({'Provider': max_provider.user_id, 'Function': (service.id - 7), 'Invocations': 1})
+
+    print(updated_data)
+
+    with open(file_path, mode='w', newline='') as csv_file:
+        fieldnames = ['Provider', 'Function', 'Invocations']
+        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+        # Write the header
+        csv_writer.writeheader()
+
+        # Write the updated rows
+        csv_writer.writerows(updated_data)
+
+    return max_provider
+
+# MAIN CODE:
+
+get_mclient()
